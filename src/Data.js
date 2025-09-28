b59892cc01dadeccc59f4427fbc08da29e7f9639
alfahad ansari <157451286+AlFahadAnsari@users.noreply.github.com>
2025-09-28 21:26:22 +0530

    scroll effect done

---
+++ src/Data.js
@@ -0,0 +1,31 @@
+import CAMILLE from "./assets/agencyImage/CAMILLE_480X640_2-480x640.jpg";
+import CarlMain from "./assets/agencyImage/Carl_480x640-480x640.jpg";
+import ChantalG from "./assets/agencyImage/ChantalG_480x640-480x640.jpg";
+import Claire from "./assets/agencyImage/Claire_480x640-480x640.jpg";
+import HugoJoseph from "./assets/agencyImage/HugoJoseph_480x640-480x640.jpg";
+import joel from "./assets/agencyImage/joel_480X640_3-480x640.jpg";
+import Lawrence from "./assets/agencyImage/Lawrence_480x640-480x640.jpg";
+import MAXIME from "./assets/agencyImage/MAXIME_480X640_2-480x640.jpg";
+import MEGGIE from "./assets/agencyImage/MEGGIE_480X640_2-480x640.jpg";
+import MEL from "./assets/agencyImage/MEL_480X640-480x640.jpg";
+import Michele from "./assets/agencyImage/Michele_480x640-480x640.jpg";
+import MyleneS from "./assets/agencyImage/MyleneS_480x640-480x640.jpg";
+import Olivier from "./assets/agencyImage/Olivier_480x640-480x640.jpg";
+import SophieA from "./assets/agencyImage/SophieA_480x640-480x640.jpg";
+
+export const imageArray = [
+  CarlMain,
+  CAMILLE,
+  ChantalG,
+  Claire,
+  HugoJoseph,
+  joel,
+  Lawrence,
+  MAXIME,
+  MEGGIE,
+  MEL,
+  Michele,
+  MyleneS,
+  Olivier,
+  SophieA,
+];
--- src/Pages/Agency.jsx
+++ src/Pages/Agency.jsx
@@ -1,8 +1,61 @@
-import React from 'react'
+import React, { useRef } from 'react'
+import { useGSAP } from '@gsap/react'
+import gsap from 'gsap'
+import { ScrollTrigger } from 'gsap/all'
+import { imageArray } from '../Data'
 
 const Agency = () => {
+
+  gsap.registerPlugin(ScrollTrigger)
+  const ImageDiv = useRef(null)
+  const ImageSrc = useRef()
+
+  // console.log(ImageSrc.current.src)
+
+  useGSAP(function () {
+    gsap.to(ImageDiv.current, {
+      scrollTrigger: {
+        trigger: ImageDiv.current,
+        // markers: true,
+        start: "top 20.50%",
+        end: "top -80%",
+        scrub: true,
+        pin: true,
+        onUpdate: (el) => {
+          // console.log(Math.floor(el.progress * imageArray.length))
+          let ImageIndex;
+          if (el.progress < 1) {
+            ImageIndex = Math.floor(el.progress * imageArray.length)
+          } else {
+            ImageIndex = imageArray.length - 1
+          }
+          ImageSrc.current.src = imageArray[ImageIndex]
+          console.log(imageArray[ImageIndex])
+        }
+      }
+    })
+  })
+
   return (
-    <div>Agency</div>
+    <div className=''>
+
+      <div ref={ImageDiv} className='h-[20vw] w-[15vw] absolute top-40 left-1/3'>
+        <img ref={ImageSrc} src={""} alt="" />
+      </div>
+
+      <div className='font-[font2] relative'>
+        <div className='mt-[50vh]'>
+          <h1 className='text-[20vw] text-center uppercase  leading-[16vw] '>SEVEN7Y
+            TWO</h1>
+        </div>
+
+        <div className='ml-[40%] '>
+          <p className='text-6xl mt-20'>
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Notre curiosité nourrit notre créativité. On reste humbles et on dit non aux gros egos, même le vôtre. Une marque est vivante. Elle a des valeurs, une personnalité, une histoire. Si on oublie ça, on peut faire de bons chiffres à court terme, mais on la tue à long terme. C’est pour ça qu’on s’engage à donner de la perspective, pour bâtir des marques influentes.
+          </p>
+        </div>
+      </div>
+    </div>
   )
 }
 
